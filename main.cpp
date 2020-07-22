611c271746295d3926c228083bc5a74050cb9552
Saussereau Dominique <domsau2@yahoo.fr>
2020-07-22 23:22:24 +0200

    Maybe basiquely finished, maybe bugs.
    Please, tell me bugs.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,27 @@
 #include <time.h>
 #include <fstream>
 #include <vector>
+#include <string>
 #include <algorithm> // https://bakura.developpez.com/tutoriel/cpp/tri/
 using namespace std;
 // https://www.youtube.com/watch?v=AgtOCNCejQ8
 
-int maxCandidate = 42;
+int maxCandidate() { return 12;}
+int membersCalc() {
+    return rand() % 5 + 3;
+    }
 
-void displayMatrix(vector < vector < int > >& Matrix) {
+int choicesCalc() {
+    return rand() % 5 + 3;
+    }
+
+void absVector1D(vector < int >& Matrix1D) {
+    for(int a = 0; a < Matrix1D.size(); a++)  {
+        Matrix1D[a] = abs(Matrix1D[a]);
+        }
+    }
+
+void displayMatrix(vector < vector < int > >& Matrix) { // Ok.
     for(int a = 0; a < Matrix.size(); a++) {
         for(int b = 0; b < Matrix[a].size(); b++) {
             cout << Matrix[a][b] << "\t" ;
@@ -18,44 +32,99 @@ void displayMatrix(vector < vector < int > >& Matrix) {
     }
 
 int value() {
-    return ((2 * (rand() % 2)  - 1) * (rand() % maxCandidate + 1));
+    return ((2 * (rand() % 2)  - 1) * (rand() % maxCandidate() + 1)); // + or -, no 0.
     }
 
 int SortByAbs(const int& a, const int& b) { // https://www.codingame.com/playgrounds/15869/c-runnable-snippets/sorting-a-vector
     return abs(a) == abs(b) ? a < b : (abs(a) < abs(b));
     }
 
-void generateMatrix(vector < vector < int > >& Matrix) {
+void generateMatrix(vector < vector < int > >& Matrix) { // Ok.
     for(int a = 0; a < Matrix.size(); a++) {
-        int choices = rand() % 5 + 1;
+        int choices = choicesCalc();
         Matrix[a][0] = value();
         for(int b = 0; b < choices; b++) {
             Matrix[a].push_back(value());
             }
+// Candidates are better presented in order:
         sort(Matrix[a].begin(), Matrix[a].end(), SortByAbs); // https://www.codingame.com/playgrounds/15869/c-runnable-snippets/sorting-a-vector
+// Delete double advices:
+restartForCleanDoubleAdvices_generateMatrix: // Sorry!
+        for(int b = 0; b < Matrix[a].size() - 1; b++) {
+            if(abs(Matrix[a][b]) == abs(Matrix[a][b + 1])) {
+                Matrix[a].erase(Matrix[a].begin() + b - 1); // Remove usually "accept", biaised for "refuse". Random can be added, but slow.
+                goto restartForCleanDoubleAdvices_generateMatrix; // Sorry!
+                }
+            }
         }
     }
 
-bool testMatrix(vector < int >&MatrixProposed, vector < vector < int > >& MatrixMatrix) {
-    clog << "TODO!" << endl;
-    return true;
+bool isIntInVector1D(int a, vector < int > & vector1D) { // Ok.
+    for(int curseur = 0; curseur < vector1D.size(); curseur++) {
+        if(a == vector1D[curseur]) {
+            return true;
+            }
+        }
+    return false;
     }
 
+bool testMember(vector < int >& MatrixProposed, vector < int > & MatrixMember) {
+    // For this member, is there a wanted candidate?
+    for(int candidat = 0; candidat < MatrixProposed.size(); candidat++) {
+        if(isIntInVector1D(MatrixProposed[candidat], MatrixMember) == true) {
+            return true;
+            }
+        }
+    // No wanted candidate here for this member.
+    // For this member, is there a refuse? If no: return false.
+    bool refuse = false;
+    for(int demande = 0; demande < MatrixMember.size(); demande++) {
+        if(MatrixMember[demande] < 0) {
+            refuse = true;
+            }
+        }
+    if(refuse == false) {
+        return false;
+        }
+// Now, there is one refuse or more for this member, but:
+// For this member, is all refused candidates? If no: return true.
+// Pex :   MatrixMember = {1, -2, -3, -4, 5}
+// Pex : MatrixProposed = {2, 13, 4}
+    for(int demande = 0; demande < MatrixMember.size(); demande++) {
+        if(MatrixMember[demande] > 0) { // Positives cases tested before.
+            continue;
+            }
+        if(isIntInVector1D(-MatrixMember[demande], MatrixProposed) == false) {
+            return true;
+            }
+        }
+    return false;
+    }
+
+bool testMatrix(vector < int >&MatrixProposed, vector < vector < int > >& Matrix) {
+    for(int membre = 0; membre < Matrix.size(); membre++) {
+        if(testMember(MatrixProposed, Matrix[membre]) == false) {
+            return false;
+            }
+        }
+    return true;
+    }
 
 int main() {
     srand(time(NULL));       // No need for better init.
     // int hardToSolve = 0; // ?
-    int   members =     rand() % 5 + 1;
-    int choices =      1;
+    int   members =  membersCalc();
+    int choices = 1;
     vector < vector < int > > Matrix(members, vector< int >(choices, 0));
     generateMatrix(Matrix);
     displayMatrix(Matrix);
-    cout << "Your proposition? (1 number at a time, a true letter to close.)" << endl;
+    cout << "Your proposition? (1 positive number at a time, a true letter to close.)" << endl;
     vector<int> MatrixProposed(0, 0);
     int input = 0;
-    while(cin >> input) { //enter any non-integer to end the loop!
+    while(cin >> input) { // Enter any letter to end the loop!
         MatrixProposed.push_back(input);
         }
+    absVector1D(MatrixProposed); // Only positive numbers! Cheaters defeated!
     if(testMatrix(MatrixProposed, Matrix)) {
         cout << "You win!" << endl;
         }
